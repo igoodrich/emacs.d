205bb99bb4e961c3720b4df2c023e64ee783a263
Ian Goodrich <igoodrich@gmail.com>
2013-04-08 23:38:20 -0700

    checkpoint

--- init.el
+++ init.el
@@ -21,14 +21,24 @@
 (require 'prelude-scss)
 (require 'prelude-xml)
 
+(menu-bar-mode -1)
+
+(setq scroll-margin 20)
 ;; These settings reduce the ass of prelude
 (setq prelude-guru nil)
 (disable-theme 'zenburn)
 (setq prelude-whitespace nil)
+(setq prelude-flyspell nil)
 
-(when (eq system-type 'darwin) ;; mac specific settings
-  (setq mac-option-modifier 'alt)
-  (setq mac-command-modifier 'meta))
+;; (when (eq system-type 'darwin) ;; mac specific settings
+;;   (setq mac-option-modifier 'alt)
+;;   (setq mac-command-modifier 'meta))
+
+;; (setq visible-bell nil)
+(defun my-bell ()
+  (message "bell"))
+
+(setq ring-bell-function 'ignore)
 
 (defun comment-or-uncomment-region-or-line ()
   "Comments or uncomments the region or the current line if there's no active region."
@@ -39,7 +49,7 @@
       (setq beg (line-beginning-position) end (line-end-position)))
     (comment-or-uncomment-region beg end)))
 (define-key global-map (kbd "A-/") 'hippie-expand)
-(define-key global-map (kbd "M-/") 'comment-or-uncomment-region-or-line)
+(define-key global-map (kbd "M-\\") 'comment-or-uncomment-region-or-line)
 
 (defun vi-open-line-above ()
   "insert a newline above the current line and put point at beginning."
@@ -66,12 +76,174 @@
   (unless mark-active 
     (beginning-of-line) 
     (push-mark (point)
-    (setq mark-active t
-          transient-mark-mode t)))
+               (setq mark-active t
+                     transient-mark-mode t)))
   (forward-line 1))
 (global-set-key (kbd "M-l") 'select-line-and-get-ready-to-select-next-line)
+(define-key global-map [(meta shift down)] 'select-line-and-get-ready-to-select-next-line)
 
-( 'rvm)
 (rvm-use-default) ;; use rvm's default ruby for the current Emacs session
 (require 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+(add-hook 'ruby-mode-hook
+       (lambda () (rvm-activate-corresponding-ruby)))
+
+(add-hook 'ruby-mode-hook
+        (lambda () ((define-key ruby-mode-map [(ctrl meta u)] 'er/ruby-backward-up)
+                    (define-key ruby-mode-map [(ctrl meta d)] 'er/ruby-backward-down))))
+
+(global-set-key (kbd "M-x") 'smex)
+
+(global-set-key (kbd "M-C-=") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-0") 'ace-jump-mode)
+
+(require 'thingatpt)
+(require 'imenu)
+
+
+(defun mine-goto-symbol-at-point ()
+  "Will navigate to the symbol at the current point of the cursor"
+  (interactive)
+  (ido-goto-symbol (thing-at-point 'symbol)))
+
+(defun ido-goto-symbol (&optional a-symbol)
+  "Will update the imenu index and then use ido to select a symbol to navigate to"
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols (symbol-list)
+                       (when (listp symbol-list)
+                         (dolist (symbol symbol-list)
+                           (let ((name nil) (position nil))
+                             (cond
+                              ((and (listp symbol) (imenu--subalist-p symbol))
+                               (addsymbols symbol))
+
+                              ((listp symbol)
+                               (setq name (car symbol))
+                               (setq position (cdr symbol)))
+
+                              ((stringp symbol)
+                               (setq name symbol)
+                               (setq position (get-text-property 1 'org-imenu-marker symbol))))
+
+                             (unless (or (null position) (null name))
+                               (add-to-list 'symbol-names name)
+                               (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    (let* ((selected-symbol
+            (if (null a-symbol)
+                (ido-completing-read "Symbol? " symbol-names)
+              a-symbol))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (cond
+       ((overlayp position)
+        (goto-char (overlay-start position)))
+       (t
+        (goto-char position))))))
+
+(global-set-key (kbd "C-x C-i") 'ido-goto-symbol)
+(global-set-key (kbd "C-x C-p") 'mine-goto-symbol-at-point)
+
+(add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
+(global-set-key (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)
+
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(setq set-mark-command-repeat-pop t)
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/rhtml-minor-mode"))
+(require 'rhtml-mode)
+
+;; while key-chord is useful on its own, it's totally necessary for evil
+(require 'key-chord)
+
+;;
+;; EVIL
+;;
+
+(setq evil-shift-width 2)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/evil")
+(require 'evil)
+(evil-mode 1)
+(require 'evil-leader)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+    "," 'evil-buffer)
+
+(require 'evil-rails)
+
+;; expand-region
+(add-to-list 'load-path "~/.emacs.d/vendor/expand-region")
+(require 'expand-region)
+;; (global-set-key (kbd "C-=") 'er/expand-region)
+
+;; Rinari
+(add-to-list 'load-path "~/.emacs.d/vendor/rinari")
+(require 'rinari)
+
+;; git-gutter
+(global-git-gutter-mode t)
+
+;;Exit insert mode by pressing j and then k quickly
+(setq key-chord-two-keys-delay 0.2)
+(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+(key-chord-define evil-normal-state-map "ss" 'save-buffer)
+(key-chord-define evil-normal-state-map "]q" 'next-error)
+(key-chord-define evil-normal-state-map "[q" 'previous-error)
+(key-chord-define evil-normal-state-map "]c" 'git-gutter:next-hunk)
+(key-chord-define evil-normal-state-map "[c" 'git-gutter:previous-hunk)
+;; (key-chord-define evil-normal-state-map ";;" 'er/expand-region)
+(key-chord-mode 1)
+
+(evil-leader/set-key "gs" 'magit-status)
+(evil-leader/set-key "gm" 'rinari-find-model)
+(evil-leader/set-key "gc" 'rinari-find-controller)
+(evil-leader/set-key "gv" 'rinari-find-view)
+(evil-leader/set-key "gj" 'rinari-find-javascript)
+(evil-leader/set-key "gt" 'rinari-find-test)
+(evil-leader/set-key "t" 'projectile-find-file)
+(evil-leader/set-key "b" 'ido-switch-buffer)
+(evil-leader/set-key "r" 'prelude-recentf-ido-find-file)
+(evil-leader/set-key "e" 'er/expand-region)
+
+;; (add-to-list 'load-path "~/.emacs.d/vendor/emacs-emamux")
+;; (require 'emamux)
+
+;; (add-to-list 'load-path "~/.emacs.d/vendor/emamux-ruby-test")
+;; (require 'emamux-ruby-test)
+
+;; This removes unsightly ^M characters that would otherwise
+;; appear in the output of java applications.
+;; (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
+;; (add-hook 'comint-output-filter-functions 'shell-strip-ctrl-m nil t)
+
+;; (require 'multi-term)
+;; (setq multi-term-program "/bin/zsh")
+
+;; Use Emacs terminfo, not system terminfo
+;; Seems to let me use my zsh crazy prompt without mucking with it
+(setq system-uses-terminfo nil)
+
+
+(setq ag-highlight-search t)
+
+
+;; TODO: this doesn't work
+(setq ruby-compilation-executable "zeus testrb")
+
+;; (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
+(require 'yasnippet)
+(setq yas-snippet-dirs '("~/.emacs.d/snippets" "~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/plugins/vendor/yasnippet/extras/imported"))
+(yas-global-mode 1)
+;; When entering rinari-minor-mode, consider also the snippets in the
+;; snippet table "rails-mode"
+;; TODO: fixme - this doesn't seem to work
+(add-hook 'rinari-minor-mode-hook
+          #'(lambda ()
+              (setq yas/mode-symbol 'rails-mode)))
+
+
